a28afc7ed83ce81b6de49211094fc1b064e3f101
zakinaA <zakina.rostand@gmail.com>
2022-09-02 13:00:27 +0200

    modif du nom de la bdd mariadb

--- src/main/java/dao/InitConnexion.java
+++ src/main/java/dao/InitConnexion.java
@@ -45,8 +45,8 @@ public class InitConnexion implements ServletContextListener {
         try
         {
             //obtention de la connexion
-            connection = DriverManager.getConnection ("jdbc:mariadb://127.0.0.1:3307/webzik","root","");
-           //connection = DriverManager.getConnection ("jdbc:mysql://localhost:3306/equida","root","");
+            connection = DriverManager.getConnection ("jdbc:mariadb://127.0.0.1:3307/normanzik","root","");
+         
             //sauvegarder la connexion dans le context
             servletContext.setAttribute("connection",connection);
             System.out.println("jdbc:mariadb://127.0.0.1:3306 - connexion ");
